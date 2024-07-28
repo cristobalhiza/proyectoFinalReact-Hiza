959507e664e7548af445f4d8b73ae04b815cb599
cristobalhiza <cristobalhiza@gmail.com>
2024-07-27 23:54:39 -0400

    fix post-build imports

--- post-build.js
+++ post-build.js
@@ -1,23 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
-const filePath = path.join(__dirname, 'dist', 'index.html');
+const filePath = path.join(process.cwd(), 'dist', 'index.html');
 let html = fs.readFileSync(filePath, 'utf-8');
 
-// Regular expression to find the script tag in the head
 const scriptRegex = /<script type="module" crossorigin src="\/assets\/[^"]+\.js"><\/script>/;
 const scriptMatch = html.match(scriptRegex);
 
 if (scriptMatch) {
   const scriptTag = scriptMatch[0];
 
-  // Remove the script tag from the head
   html = html.replace(scriptTag, '');
 
-  // Inject the script tag before the closing body tag
   html = html.replace('</body>', `${scriptTag}</body>`);
 }
 
-// Write the modified HTML back to the file
 fs.writeFileSync(filePath, html, 'utf-8');
 console.log('Post-build script executed successfully.');
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import moveScript from './vite-plugin-move-script'
 
 export default defineConfig({
-  plugins: [react(), moveScript()],
+  plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.png'],
   build: {
     sourcemap: false,
